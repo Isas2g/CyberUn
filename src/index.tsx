945ceafc4b02f1374186cc5608f33c8f5164d509
Renat <parabell241@gmail.com>
2022-10-10 00:47:14 +0300

    fix: пофикшены роуты

--- src/index.tsx
+++ src/index.tsx
@@ -22,7 +22,9 @@ const router = createBrowserRouter([
 
 root.render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>
 );
 
